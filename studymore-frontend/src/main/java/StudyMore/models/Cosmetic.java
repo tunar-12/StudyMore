9069078a01635017586db8887c9f84fc216617c8
Hassaan Aslam <Hassaan.aslam16@gmail.com>
2026-03-30 15:59:32 +0500

    Implemented Cosmetic class

--- studymore-frontend/src/main/java/StudyMore/models/Cosmetic.java
+++ studymore-frontend/src/main/java/StudyMore/models/Cosmetic.java
@@ -1,5 +1,39 @@
 package StudyMore.models;
 
 public class Cosmetic {
+    private String name;
+    private CosmeticType type;
+    private int price;
+    private String imagePath;
+    private String description;
+    private Long cosmeticId;
+
+    public Cosmetic(Long cosmeticId, String name, CosmeticType type, int price, String imagePath, String description) {
+        this.cosmeticId = cosmeticId;
+        this.name = name;
+        this.type = type;
+        this.price = price;
+        this.imagePath = imagePath;
+        this.description = description;
+    }
+
+    public String getName() { 
+        return name; 
+    }
     
+    public CosmeticType getType() { 
+        return type; 
+    }
+    
+    public int getPrice() { 
+        return price; 
+    }
+    
+    public String getImagePath() { 
+        return imagePath; 
+    }
+    
+    public String getDescription() { 
+        return description; 
+    }
 }
