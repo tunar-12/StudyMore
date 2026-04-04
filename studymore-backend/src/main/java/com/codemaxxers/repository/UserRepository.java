d08d146ecd4044f6f6db78335224090bc3157b9e
tunar-12 <tunarb2020@gmail.com>
2026-04-04 15:53:52 +0300

    Added findByUsernameContainingIgnoreCase to UserRepository in backend

--- studymore-backend/src/main/java/com/codemaxxers/repository/UserRepository.java
+++ studymore-backend/src/main/java/com/codemaxxers/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.codemaxxers.repository;
 import com.codemaxxers.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
+    List<User> findByUsernameContainingIgnoreCase(String keyword);
 }
