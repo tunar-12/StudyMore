361576a86d92d3cadccf63733077c31f855035e4
tunar-12 <tunarb2020@gmail.com>
2026-04-04 15:40:05 +0300

    AchievementRepository fixed in backend

--- studymore-backend/src/main/java/com/codemaxxers/repository/AchievementRepository.java
+++ studymore-backend/src/main/java/com/codemaxxers/repository/AchievementRepository.java
@@ -1,9 +1,14 @@
 package com.codemaxxers.repository;
 
 import com.codemaxxers.model.Achievement;
+import com.codemaxxers.model.enums.AchievementType;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AchievementRepository extends JpaRepository<Achievement, Long> {
+    List<Achievement> findByType(AchievementType type);
 }
